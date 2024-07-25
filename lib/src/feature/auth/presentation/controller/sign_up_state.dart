98f442ef27c433470c0ca66d7d1e29f832fc1e25
Yubron <wlstn4966@gmail.com>
2024-07-25 17:31:41 +0900

    ✨feat: signUpFirstStepPage validation
    - nickname, gender, ageRange가 공백 혹은 null일 경우 다음 버튼 비활성화

--- lib/src/feature/auth/presentation/controller/sign_up_state.dart
+++ lib/src/feature/auth/presentation/controller/sign_up_state.dart
@@ -2,8 +2,12 @@ part of '../../../../generated_files/controller.dart';
 
 @freezed
 sealed class SignUpPageState with _$SignUpPageState {
+  const SignUpPageState._();
+
   factory SignUpPageState({
     required SignUpEntity signUpEntity,
     required List<InterestCategoryEntity> allInterestCategory,
+    @Default(false) bool canPressNextButton,
+    @Default(false) bool canPressSignUpButton,
   }) = _SignUpPageState;
 }
