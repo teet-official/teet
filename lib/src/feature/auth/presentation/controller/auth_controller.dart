fa7508e1523cfd6de847a9c69d155cd95a4a211b
Yubron <wlstn4966@gmail.com>
2024-07-30 00:25:39 +0900

    ✨feat: onInitSignIn 함수 수정
    - 로그아웃 후 해당 함수가 실행되는데 토큰 및 세션이 없어 getUser함수에서 exception 발생

--- lib/src/feature/auth/presentation/controller/auth_controller.dart
+++ lib/src/feature/auth/presentation/controller/auth_controller.dart
@@ -88,6 +88,10 @@ class AuthController extends _$AuthController {
   }
 
   Future<void> onInitSignIn() async {
+    if (!state.isSignIn) {
+      return;
+    }
+
     final userResponse = await Supabase.instance.client.auth.getUser();
     final user = userResponse.user;
     if (user == null) {
